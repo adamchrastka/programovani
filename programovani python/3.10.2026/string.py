2c1fd9719a6d495fa1d98447b14719a2ad46c1db
adamchrastka <adam.chratka@gmail.com>
2026-03-10 09:38:15 +0100

    ahoj

--- programovani python/3.10.2026/string.py
+++ programovani python/3.10.2026/string.py
@@ -1,11 +1,18 @@
 string = str(input("Zadejte heslo: "))
+cislo = 0
 if len(string) < 8:
-    print("heslo je nejslabsi.")
+    
 elif not any(char.isupper() for char in string):
-    print("heslo je slabe")
+    cislo = cislo + 1
 elif not any(char.islower() for char in string):
-    print("heslo je min slabe")
+    cislo = cislo + 1
 elif not any(char.isdigit() for char in string):
-    print("heslo je stredne slabe")
+    cislo = cislo + 1
 elif not any(char in "!@#$%^&*()-_=+[]{}|;:,.<>?/" for char in string):
-    print("heslo neni slabe")
+    cislo = cislo + 1
+if cislo == 0:
+    print("Heslo je silné.")
+elif cislo == 1:
+    print("Heslo je střední.")
+else:
+    print("Heslo je slabé.")
